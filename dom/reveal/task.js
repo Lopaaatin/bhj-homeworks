9ba6278f2b4cb864af0b66395333ef4c2110f35e
Lopaaatin <img.russia@gmail.com>
2022-08-28 16:37:57 +0700

    добавлено исчезновение блоков при дальнейшей прокрутке

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -5,5 +5,8 @@ document.addEventListener('scroll', function() {
     if (reveal.getBoundingClientRect().top < window.innerHeight) {
       reveal.classList.add('reveal_active');
     }
+    if ((reveal.getBoundingClientRect().top > window.innerHeight) || (reveal.getBoundingClientRect().bottom < 0)) {
+      reveal.classList.remove('reveal_active');
+    }
   });
 });
