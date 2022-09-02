d7a587e3887860fee6df904728809c814371436b
Lopaaatin <img.russia@gmail.com>
2022-09-02 14:33:24 +0700

    Вторая попытка сдать задачу

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,21 +1,23 @@
 let input = document.getElementById('task__input');
 let tasksList = document.getElementById('tasks__list');
+let button = document.getElementById('tasks__add');
 
-input.addEventListener('change', () => {
+button.addEventListener('click', (event) => {
+  event.preventDefault();
   if (input.value.trim()) {
     let newTask = `<div class="task">
                     <div class="task__title">
-                      ${input.value}
+                      ${input.value.trim()}
                     </div>
                     <a href="#" class="task__remove">&times;</a>
                   </div>`;
     tasksList.insertAdjacentHTML('beforeend', newTask);
     input.value = null;
-  }
 
   let removeButtons = Array.from(tasksList.querySelectorAll('.task__remove'));
   removeButtons[removeButtons.length - 1].addEventListener('click', (event) => {
     event.preventDefault();
     removeButtons[removeButtons.length - 1].closest('.task').remove();
   });
+  }
 });
